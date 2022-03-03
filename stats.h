41f6e6cfb1431a3db50a85bc6aa816769263ed92
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-03-03 18:32:37 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,10 +1,5 @@
-struct Stats
-  {
-  float average;
-  float min;
-  float max;
-};
-Stats compute_statistics(const float* numberset, int setlength);
+
+struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
