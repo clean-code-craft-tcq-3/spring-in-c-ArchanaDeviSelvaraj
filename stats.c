aecd8836c63e284dda7ff79553ec52e8154ec078
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-03-03 15:49:25 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,6 +1,6 @@
 #include "stats.h"
 
-struct Stats compute_statistics(float numberset[], int setlength) {
+Stats compute_statistics(float numberset[], int setlength) {
     struct Stats s;
     s.average = 0;
     s.min = numberset[0];
