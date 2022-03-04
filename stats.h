62e1027af22e1205dbcc7ad71844f8f43e1d96db
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-03-04 14:47:39 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -4,6 +4,6 @@ struct Stats compute_statistics(const float* , int );
 typedef int (*alerter_funcptr)(int,int);
 
 //int check_and_alert(float maxThreshold, struct Stats computedStats);
-int check_and_alert(float , struct );
+int check_and_alert(float , struct Stats);
 //int check_and_alert_1(float maxThreshold, struct Stats computedStats);
-int check_and_alert_1(float , struct );
+int check_and_alert_1(float , struct Stats);
