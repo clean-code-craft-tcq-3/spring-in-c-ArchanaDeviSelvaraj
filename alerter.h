ae1930c578de24189619d1f17e2da538cc0b5a9d
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-03-04 10:48:20 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,1 +1,6 @@
+typedef void (*alerter_funcptr)();
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
 
