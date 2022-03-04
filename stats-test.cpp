b4beca270861aa7e51468caff01a8f0d4fb636a9
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-03-04 12:23:46 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -3,8 +3,8 @@
 #include "catch.hpp"
 #include "stats.h"
 #include "stats.c"
-#include "alerter.c"
-#include "alerter.h"
+//#include "alerter.c"
+//#include "alerter.h"
 
 #include <stdlib.h>
 #include <math.h>
