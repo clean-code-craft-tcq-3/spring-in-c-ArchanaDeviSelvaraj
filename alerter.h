b8345001bd233aecf9e40cefd31bf9f756758944
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-03-04 11:04:16 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,6 +1,6 @@
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
-check_and_alert(maxThreshold, alerters, computedStats);
+
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
