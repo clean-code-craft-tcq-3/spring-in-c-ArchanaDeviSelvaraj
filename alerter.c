7c27580d397b56309a7c05527e47b8cf12904b1a
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-03-04 11:59:24 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -3,7 +3,7 @@
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
-int check_and_alert(float maxThreshold, alerters, computedStats)
+int check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
   if(computedStats.max>maxThreshold)
   {
