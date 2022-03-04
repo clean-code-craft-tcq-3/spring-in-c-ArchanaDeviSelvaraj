4211f5271181f00252ae8810c1336779a7fb1f6d
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-03-04 11:09:28 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,8 +1,7 @@
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
- alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
-
+ 
 check_and_alert(maxThreshold, alerters, computedStats)
 {
 }
